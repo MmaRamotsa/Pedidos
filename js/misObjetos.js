d65fe4c7258583c3f94f91802375864be2dd642d
Marian Martínez <ma.martinez68@gmail.com>
2017-02-27 10:39:46 +0100

    Añadidos todos los métodos set del objeto Pedido. MAMG

--- js/misObjetos.js
+++ js/misObjetos.js
@@ -1,7 +1,7 @@
 // ===================================================
 // Página creada por : Maria Antonia Martínez García
 // Fecha: 27/02/2017
-// getsión : v1.0
+// versión : v2.0
 // ===================================================
 // Descripción: Un pedido está compuesto de los siguientes datos:
 // - Una fecha de pedido
@@ -15,37 +15,48 @@ function Pedido(fechaPedido,
 								totalPedido,
 								dirEntrega,
 								dirFacturacion){
-	//Estado
+	//Propiedades (Estado)
 	this.fechaPedido = fechaPedido;
 	this.numArticulos = numArticulos;
 	this.totalPedido = totalPedido;
 	this.dirEntrega = dirEntrega;
 	this.dirFacturacion = dirFacturacion;
-	//Comportamiento
+	// Métodos (Comportamiento)
 	// Método para conocer la fecha del pedido.
 	this.getFecha = function (){
 		return this.fechaPedido;
+	}
+	//Método para modificar la fecha del pedido
+	this.setFechaPedido = function(nuevaFechaPedido){
+		this.fechaPedido= nuevaFechaPedido;
 	}
 	// Método para conocer el número de artículos del pedido.
 	this.getNumArticulos = function (){
 		return this.numArticulos;
 	}
+	//Método para modificar el número de artículos del pedido
+	this.setNumArticulos = function(nuevoNumArticulos){
+		this.numArticulos= nuevoNumArticulos;
+	}
 	// Método para conocer el total del pedido.
 	this.getTotal = function (){
 		return this.totalPedido;
+	}
+	//Método para modificar el total del pedido
+	this.setTotal = function(nuevoTotal){
+		this.totalPedido= nuevoTotal;
 	}
 	// Método para conocer la dirección de entrega del pedido.
 	this.getDirEntrega = function (){
 		return this.dirEntrega;
 	}
-	// Método para conocer la dirección de facturación del pedido.
-	this.getDirFacturacion = function (){
-		return this.dirFacturacion;
-	}
-
 	//Método para modificar la dirección de entrega
 	this.setDirEntrega = function(nuevaDirEntrega){
 		this.dirEntrega = nuevaDirEntrega;
+	}
+	// Método para conocer la dirección de facturación del pedido.
+	this.getDirFacturacion = function (){
+		return this.dirFacturacion;
 	}
 	//Método para modificar la dirección de facturación
 	this.setDirFacturacion = function(nuevaDirFacturacion){
